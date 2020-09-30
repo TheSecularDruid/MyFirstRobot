10a09f4633080a7b162c773a03ec9cf1040a9cf9
adnribeiro <adnribeiro@ensc.fr>
2020-09-30 16:01:56 +0200

    Update testMotor.py

--- testMotor.py
+++ testMotor.py
@@ -7,11 +7,11 @@ if not ports:
 dxl_io = pypot.dynamixel.DxlIO(ports[1])
 dxl_io.set_wheel_mode([1])
 t0 = time.time()
-    while True:
-        t = time.time()
-        if (t - t0) > 5:
-            break
+while True:
+    t = time.time()
+    if (t - t0) > 5:
+        break
 
-        dxl_io.set_moving_speed({1: 10})
+    dxl_io.set_moving_speed({1: 10})
 
-        time.sleep(5)
+    time.sleep(5)
