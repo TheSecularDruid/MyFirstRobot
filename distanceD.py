cf741ac117af1b54e434b372c4b844355b0b89e6
adnribeiro <adnribeiro@ensc.fr>
2020-09-30 17:25:05 +0200

    Update distanceD.py

--- distanceD.py
+++ distanceD.py
@@ -38,12 +38,12 @@ if __name__ == '__main__':
 d=int(input("entrer une distance"))
 
 
-dxl_io1 = pypot.dynamixel.DxlIO(ports[1])
+dxl_io1 = pypot.dynamixel.DxlIO(ports[0])
 dxl_io1.set_wheel_mode([1])
 dxl_io1.set_moving_speed({1: 10})
 dxl_io1.set_goal_position(d)
 
-dxl_io2 = pypot.dynamixel.DxlIO(ports[2])
+dxl_io2 = pypot.dynamixel.DxlIO(ports[1])
 dxl_io2.set_wheel_mode([1])
 dxl_io2.set_moving_speed({1: 10})
 dxl_io2.set_goal_position(d)
