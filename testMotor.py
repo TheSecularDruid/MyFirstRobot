3cff34b1995f579ee967288cd4b7b0692fd2f872
adnribeiro <adnribeiro@ensc.fr>
2020-10-01 14:37:37 +0200

    Update testMotor.py

--- testMotor.py
+++ testMotor.py
@@ -48,8 +48,11 @@ class Motor():
 
 if __name__ == "__main__":
     m = Motor()
-    m.set_speed_left_wheels(-60)
-    m.set_speed_right_wheels(60)
+    m.set_speed_left_wheels(60)
+    m.set_speed_right_wheels(-60)
+    time.sleep(10)
+    m.set_speed_left_wheels(60)
+    m.set_speed_right_wheels(-30)
     time.sleep(10)
     m.set_speed_right_wheels(0)
     m.set_speed_left_wheels(0)
