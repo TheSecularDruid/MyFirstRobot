af87f6ad3a35d7380c7eb769c48a9f711450709e
adnribeiro <adnribeiro@ensc.fr>
2020-10-01 13:58:49 +0200

    Update testMotor.py

--- testMotor.py
+++ testMotor.py
@@ -7,16 +7,18 @@ print('available ports:', ports)
 
 dxl_io = pypot.dynamixel.DxlIO(ports[0])
 dxl_io.set_wheel_mode([1,2])
-found_ids = dxl_io.scan(range(10))
-print('Found ids:', found_ids)
-t0 = time.time()
-while True:
-    t = time.time()
-    if (t - t0) > 5:
-        break
+dxl_io.set_moving_speed({2: 40})
 
-    dxl_io.set_moving_speed({2: 40})
+# found_ids = dxl_io.scan(range(10))
+# print('Found ids:', found_ids)
+# t0 = time.time()
+# while True:
+#     t = time.time()
+#     if (t - t0) > 5:
+#         break
+
+#     dxl_io.set_moving_speed({2: 40})
 
     
 
-    time.sleep(5)
+#     time.sleep(5)
